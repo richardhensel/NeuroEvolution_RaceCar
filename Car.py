74adbf8f3cc73a32626ca95f9b53cc08aa67870a
richardhensel <richardhensel20@gmail.com>
2017-03-01 19:44:39 +1000

    made track and control inputs

--- Car.py
+++ Car.py
@@ -15,10 +15,12 @@ class Car():
         self.velocity = 0.0             # rate of change of positon in direction of travel ms-1
         self.accel = 0.0               #Rate of change of velocity ms-2
         self.steering =  0.0         #rate of change of yaw radm-2 -ve left, +ve right
-
+        self.steering_rate =  0.0         #rate of change of yaw radm-2 -ve left, +ve right
+        self.max_steering = 0.00015
+    
         #Drag parameters
-        self.steeringDrag = 0.3
-        self.rollingDrag = 0.1
+        self.steering_drag = 0.9
+        self.rolling_drag = 0.1
 
         # Trip metrics
         self.dist_travelled = 0.0       # total distance travelled
@@ -35,122 +37,25 @@ class Car():
 
         #Define the sensor array
         self.sensor_origin_dist = 0.5
-        self.max_sensor_length = 300.0
+        self.max_sensor_length = 500.0
         self.num_sensors = 8
         self.sensor_measurements = [self.max_sensor_length] * self.num_sensors
 
         #Set up NN
-    
-    def __update_geometry(self):
-    #Geometry points
-        self.rear_left = self.position + self.orientation.rotate_around( euclid.Vector3(0.,0.,1.), -0.5*math.pi)* self.width/2 - self.orientation * self.origin_dist
-        self.rear_right = self.position + self.orientation.rotate_around( euclid.Vector3(0.,0.,1.), 0.5*math.pi)* self.width/2  - self.orientation * self.origin_dist
-        self.front_left = self.rear_left + self.orientation * self.length
-        self.front_right = self.rear_right + self.orientation * self.length
-        self.sensor_origin = self.position + self.sensor_origin_dist * self.length * self.orientation # sensor origin is 1 quarter of the length from the front of the car
 
-    def __get_sensor_vectors(self, ray_lengths):
-        angle = math.pi / (self.num_sensors+1)
-        vectors = []
-        count = 1
-        for i in range(0,self.num_sensors):
-            p1 = self.sensor_origin
-            # sensor vector is count*angle radians from the right of the car, with an origin at sensor origin and a length of sensor_length 
-            p2 = self.sensor_origin + self.orientation.rotate_around(euclid.Vector3(0.,0.,1.), 0.5*math.pi - count * angle) * ray_lengths[i]
+    def control(self, acceleration, steering_rate):
+        if self.crashed == False:
+            self.accel = acceleration
+            self.steering_rate = steering_rate
 
-            vectors.append((p1,p2)) 
-            count +=1
-        return vectors
-
-    def move(self, time_delta):
-        if self.crashed ==True:
-            self.velocity = 0.0
-            self.accel = 0.0
-
-        self.position += self.velocity * self.orientation * time_delta + 0.5 * (self.accel * self.orientation * (time_delta**2))
-        self.velocity += self.accel * time_delta
-        self.velocity -= self.velocity * self.rollingDrag * time_delta
-        self.velocity -= abs(self.steering) * self.velocity * self.steeringDrag * time_delta
-        self.orientation = self.orientation.rotate_around(euclid.Vector3(0.,0.,1.),self.steering * self.velocity)
-      
-        if self.crashed != True: 
-            self.dist_travelled += abs(self.position - self.prev_position)/1000.0
-            self.total_time += time_delta
-            self.avg_velocity = self.dist_travelled/self.total_time
-            print self.dist_travelled , self.total_time, self.avg_velocity
-
-        self.prev_position = self.position.copy()
-
+    def update(self, time_delta, obstacles):
+        self.__reset_sensors()
+        self.__update_dynamics(time_delta)
         self.__update_geometry()
 
-    def get_line_intersection(self, p1, p2, p3, p4):
-        s1 = (p2[0] - p1[0], p2[1] - p1[1])
-        s2 = (p4[0] - p3[0], p4[1] - p3[1])
-
-        s = (-s1[1] * (p1[0] - p3[0]) + s1[0] * (p1[1] - p3[1])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
-        t = ( s2[0] * (p1[1] - p3[1]) - s2[1] * (p1[0] - p3[0])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
-
-        if (s >= 0 and s <= 1 and t >= 0 and t <= 1):
-
-            i_x = p1[0] + (t * s1[0])
-            i_y = p1[1] + (t * s1[1])
-            return i_x, i_y # Collision detected
-        else:
-            return None, None # No collision
-
-    def sense(self, obstacle):
-#        self.sensor_ranges = []
-        sensor_vectors = self.__get_sensor_vectors([self.max_sensor_length] * self.num_sensors)
-        for i in range(0,len(sensor_vectors)):
-            intersect_list = []
-#            min_range = self.sensor_length
-
-            ray = sensor_vectors[i]
-            #unpack the start and end points of the sensor ray
-            p1 = (ray[0].x, ray[0].y)
-            p2 = (ray[1].x, ray[1].y)
-            for j in range(0,len(obstacle)):
-                if j ==0:
-                    p3 = obstacle[-1]
-                else:
-                    p3 = obstacle[j-1]
-                p4 = obstacle[j]
-                #Get intersection point in global frame
-                ix, iy = self.get_line_intersection(p1, p2, p3, p4)
-                #Append all non-null intersections to the list of intersections for that ray
-                if ix != None and iy != None:
-                    intersect_list.append(euclid.Vector3(ix,iy, 0.0))
-            # find the minimum distance. range must be shorter than the current stored min range to be stored. 
-            for vector in intersect_list:
-                measured_range = abs(vector - ray[0])
-                if measured_range < self.sensor_measurements[i]:
-                    self.sensor_measurements[i] = measured_range
-        #print self.sensor_measurements
-    def detect_collision(self, obstacle):
-        car_bounds = [self.rear_left, self.front_left, self.front_right, self.rear_right]
-        for i in range(0,len(car_bounds)):
-            if i ==0:
-                p1 = car_bounds[-1]
-            else:
-                p1 = car_bounds[i-1]
-            p2 = car_bounds[i]
-
-            for j in range(0,len(obstacle)):
-                if j ==0:
-                    p3 = obstacle[-1]
-                else:
-                    p3 = obstacle[j-1]
-                p4 = obstacle[j]
-                
-                #Get intersection point in global frame
-                ix, iy = self.get_line_intersection(p1, p2, p3, p4)
-                if ix != None or iy != None:
-                    self.crashed = True
-                    return 0
-                    
-        
-        #Test for collision between the obstacle and each of the corners/lines of the car
-        #self.crashed = True
+        for obstacle in obstacles:
+            self.__sense(obstacle)
+            self.__detect_collision(obstacle)
 
     def display(self, screen_handle):
         rx, ry = int(self.position.x), int(self.position.y)
@@ -168,16 +73,136 @@ class Car():
         sensor_vectors = self.__get_sensor_vectors(self.sensor_measurements)
         for line in sensor_vectors:
             point_list = [(line[0].x, line[0].y),(line[1].x, line[1].y)]
-            pygame.draw.lines(screen_handle, (0,0,255), False, point_list, 1)    
+            pygame.draw.lines(screen_handle, (0,0,255), False, point_list, 1)
 
-        #Draw sensor intersects
-        #for point in self.vector_list:
-        #    pygame.draw.circle(screen_handle, (255,0,0), (int(point[0]), int(point[1])), 2, 0)
-    
-    def reset_sensors(self):
+            #Draw the intersection if detected
+            pygame.draw.circle(screen_handle, (255,0,0), (int(line[1].x), int(line[1].y)), 2, 0)
+
+
+    def __reset_sensors(self):
         self.sensor_measurements = [self.max_sensor_length] * self.num_sensors
 
-    def control(self, acceleration, steering):
-        if self.crashed == False:
-            self.accel = acceleration
-            self.steering = steering
+    def __update_dynamics(self, time_delta):
+        # stay still if crashed
+        if self.crashed ==True:
+            self.velocity = 0.0
+            self.accel = 0.0
+
+        # store previous position for distance accumulation
+        self.prev_position = self.position.copy()
+
+        # update rates
+        self.steering += self.steering_rate * time_delta
+
+        self.velocity += self.accel * time_delta
+        self.velocity -= self.velocity * self.rolling_drag * time_delta
+        self.velocity -= self.velocity * abs(self.steering) * self.steering_drag * time_delta
+ 
+        if self.steering > self.max_steering:
+            self.steering = self.max_steering
+        elif self.steering < -1*self.max_steering:
+            self.steering = -1*self.max_steering
+
+        # update pose
+        self.orientation = self.orientation.rotate_around(euclid.Vector3(0.,0.,1.),self.steering * self.velocity)
+        self.position += self.velocity * self.orientation * time_delta + 0.5 * (self.accel * self.orientation * (time_delta**2))
+        
+        # Accumulate trip metrics
+        if self.crashed == False: 
+            self.dist_travelled += abs(self.position - self.prev_position)/1000.0
+            self.total_time += time_delta
+            self.avg_velocity = self.dist_travelled/self.total_time
+
+    def __update_geometry(self):
+    #Geometry points
+        self.rear_left = self.position + self.orientation.rotate_around( euclid.Vector3(0.,0.,1.), -0.5*math.pi)* self.width/2 - self.orientation * self.origin_dist
+        self.rear_right = self.position + self.orientation.rotate_around( euclid.Vector3(0.,0.,1.), 0.5*math.pi)* self.width/2  - self.orientation * self.origin_dist
+        self.front_left = self.rear_left + self.orientation * self.length
+        self.front_right = self.rear_right + self.orientation * self.length
+        self.sensor_origin = self.position + self.sensor_origin_dist * self.length * self.orientation # sensor origin is 1 quarter of the length from the front of the car
+
+    def __sense(self, obstacle):
+        sensor_vectors = self.__get_sensor_vectors([self.max_sensor_length] * self.num_sensors)
+        for i in range(0,len(sensor_vectors)):
+            intersect_list = []
+
+            ray = sensor_vectors[i]
+            #unpack the start and end points of the sensor ray
+            p1 = (ray[0].x, ray[0].y)
+            p2 = (ray[1].x, ray[1].y)
+            for j in range(0,len(obstacle)):
+                if j ==0:
+                    p3 = obstacle[-1]
+                else:
+                    p3 = obstacle[j-1]
+                p4 = obstacle[j]
+                #Get intersection point in global frame
+                ix, iy = self.__get_line_intersection(p1, p2, p3, p4)
+                #Append all non-null intersections to the list of intersections for that ray
+                if ix != None and iy != None:
+                    intersect_list.append(euclid.Vector3(ix,iy, 0.0))
+            # find the minimum distance. range must be shorter than the current stored min range to be stored. 
+            for vector in intersect_list:
+                measured_range = abs(vector - ray[0])
+                if measured_range < self.sensor_measurements[i]:
+                    self.sensor_measurements[i] = measured_range
+        #print self.sensor_measurements
+
+    def __detect_collision(self, obstacle):
+        car_bounds = [self.rear_left, self.front_left, self.front_right, self.rear_right]
+        for i in range(0,len(car_bounds)):
+            if i ==0:
+                p1 = (car_bounds[-1].x, car_bounds[-1].y)
+            else:
+                p1 = (car_bounds[i-1].x, car_bounds[i-1].y)
+            p2 = (car_bounds[i].x, car_bounds[i].y)
+
+            for j in range(0,len(obstacle)):
+                if j ==0:
+                    p3 = obstacle[-1]
+                else:
+                    p3 = obstacle[j-1]
+                p4 = obstacle[j]
+               
+                #Get intersection point in global frame
+                ix, iy = self.__get_line_intersection(p1, p2, p3, p4)
+                if ix != None or iy != None:
+                    self.crashed = True
+                    return 0
+                    
+        
+        #Test for collision between the obstacle and each of the corners/lines of the car
+        #self.crashed = True
+
+    
+    def __get_sensor_vectors(self, ray_lengths):
+        angle = math.pi / (self.num_sensors+1)
+        vectors = []
+        count = 1
+        for i in range(0,self.num_sensors):
+            p1 = self.sensor_origin
+            # sensor vector is count*angle radians from the right of the car, with an origin at sensor origin and a length of sensor_length 
+            p2 = self.sensor_origin + self.orientation.rotate_around(euclid.Vector3(0.,0.,1.), 0.5*math.pi - count * angle) * ray_lengths[i]
+
+            vectors.append((p1,p2)) 
+            count +=1
+        return vectors
+
+    def __get_line_intersection(self, p1, p2, p3, p4):
+        try:
+            s1 = (p2[0] - p1[0], p2[1] - p1[1])
+            s2 = (p4[0] - p3[0], p4[1] - p3[1])
+
+            s = (-s1[1] * (p1[0] - p3[0]) + s1[0] * (p1[1] - p3[1])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
+            t = ( s2[0] * (p1[1] - p3[1]) - s2[1] * (p1[0] - p3[0])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
+
+            if (s >= 0 and s <= 1 and t >= 0 and t <= 1):
+
+                i_x = p1[0] + (t * s1[0])
+                i_y = p1[1] + (t * s1[1])
+                return i_x, i_y # Collision detected
+            else:
+                return None, None # No collision
+        except:
+            return None, None
+
--- run.py
+++ run.py
@@ -2,40 +2,12 @@ import os, sys, math, pygame, pygame.mixer
 import random
 import euclid
 from pygame.locals import *
+from pygame.key import *
 
 from Car import Car
 
 
-# inputs: four tuples each representing a line segment point
-
-#Line1: p1,p2
-#Line2: p3,p4
-
-def get_line_intersection(p1, p2, p3, p4):
-    s1 = (p2[0] - p1[0], p2[1] - p1[1])
-    s2 = (p4[0] - p3[0], p4[1] - p3[1])
-
-    s = (-s1[1] * (p1[0] - p3[0]) + s1[0] * (p1[1] - p3[1])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
-    t = ( s2[0] * (p1[1] - p3[1]) - s2[1] * (p1[0] - p3[0])) / (-s2[0] * s1[1] + s1[0] * s2[1]);
-
-    if (s >= 0 and s <= 1 and t >= 0 and t <= 1):
-        
-        i_x = p1[0] + (t * s1[0])
-        i_y = p1[1] + (t * s1[1])
-        return i_x, i_y # Collision detected
-
-    else:
-        return 0 # No collision
-
-
-
-black = 0,0,0
-white = 255,255,255
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 0, 255
-
-screenSize = screenWidth, screenHeight = 1000, 700
+screenSize = screenWidth, screenHeight = 2000, 1200
 
 # Set the screen size
 screen = pygame.display.set_mode(screenSize)
@@ -44,41 +16,85 @@ clock = pygame.time.Clock()
 fpsLimit = 60
 runMe = True
 
-car = Car(euclid.Vector3(300., 200., 0.), euclid.Vector3(1.,0., 0.))
+left_barrier = [(980, 20), (1000.0, 20.0), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (980, 20), (981, 20), (995, 19), (1023, 20), (1065, 25), (1120, 33), (1189, 37), (1267, 33), (1341, 45), (1411, 63), (1479, 72), (1542, 65), (1603, 60), (1662, 60), (1714, 80), (1752, 118), (1771, 164), (1772, 212), (1762, 256), (1746, 296), (1730, 333), (1717, 371), (1718, 417), (1737, 456), (1766, 484), (1799, 504), (1832, 523), (1860, 556), (1875, 598), (1874, 641), (1863, 685), (1840, 728), (1805, 779), (1769, 839), (1735, 903), (1696, 973), (1641, 1038), (1569, 1084), (1489, 1106), (1409, 1104), (1338, 1083), (1277, 1046), (1229, 997), (1196, 941), (1176, 882), (1163, 824), (1153, 769), (1137, 718), (1116, 672), (1090, 631), (1058, 593), (1026, 554), (995, 513), (955, 479), (905, 467), (854, 480), (812, 509), (783, 548), (761, 589), (739, 634), (717, 683), (700, 731), (679, 774), (653, 811), (623, 841), (590, 866), (555, 886), (510, 901), (459, 910), (410, 909), (359, 909), (309, 911), (257, 909), (204, 896), (157, 866), (130, 820), (119, 771), (117, 723), (112, 677), (100, 632), (86, 585), (81, 537), (79, 487), (78, 433), (83, 374), (106, 318), (153, 280), (212, 273), (272, 277), (333, 264), (377, 225), (396, 172), (405, 119), (425, 72), (462, 40), (505, 25), (548, 21), (590, 20), (632, 21), (674, 20), (713, 15), (750, 12), (789, 11), (829, 12), (869, 17), (910, 18)]
 
-obstacle1 = [(400,100), (400, 300), (500, 200)]
-obstacle2 = [(500,500), (900, 500), (400, 700)]
+right_barrier = [(980, 120), (1000.0, 120.0), (980, 120), (984, 120), (1002, 120), (1028, 119), (1068, 120), (1122, 122), (1186, 123), (1250, 126), (1309, 136), (1363, 156), (1414, 170), (1466, 168), (1514, 166), (1560, 172), (1598, 196), (1623, 232), (1632, 271), (1625, 308), (1611, 341), (1599, 372), (1592, 406), (1592, 439), (1599, 471), (1612, 498), (1629, 522), (1645, 545), (1658, 567), (1675, 585), (1692, 602), (1705, 622), (1714, 650), (1716, 679), (1710, 707), (1695, 734), (1673, 755), (1653, 777), (1638, 802), (1624, 828), (1610, 856), (1594, 887), (1570, 917), (1537, 937), (1498, 953), (1458, 957), (1419, 951), (1384, 930), (1357, 901), (1339, 866), (1328, 827), (1318, 789), (1305, 754), (1294, 719), (1278, 683), (1260, 649), (1241, 613), (1223, 578), (1203, 541), (1182, 505), (1159, 470), (1138, 437), (1114, 406), (1089, 380), (1067, 353), (1043, 331), (1013, 314), (981, 304), (946, 304), (912, 312), (879, 326), (851, 347), (833, 374), (820, 402), (802, 425), (780, 443), (755, 462), (728, 478), (701, 498), (674, 519), (649, 541), (628, 569), (606, 599), (585, 627), (563, 655), (541, 688), (518, 720), (490, 751), (457, 781), (417, 804), (372, 811), (329, 805), (291, 791), (257, 771), (232, 743), (219, 707), (216, 669), (219, 631), (224, 593), (227, 556), (228, 519), (233, 480), (247, 443), (269, 409), (298, 382), (331, 366), (365, 356), (399, 353), (430, 346), (457, 332), (477, 311), (493, 288), (500, 263), (505, 239), (511, 216), (520, 195), (537, 174), (559, 156), (587, 145), (618, 140), (650, 137), (684, 138), (719, 135), (754, 129), (792, 124), (827, 118), (861, 113), (893, 111), (924, 112)]
 
-car.control(30.0, 0.0001)
+max_accel = 60
+max_steering_rate = 0.0005
+
+clock_tick = 0.5
+timer = 0.0
+old_timer = 0.0
+
+car_x = screenWidth/2.0
+car_y = 80.0
 
 while runMe:
 
-    #get user input
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            runMe = False
+    car = Car(euclid.Vector3(car_x, car_y, 0.), euclid.Vector3(-1.,0., 0.))
+   ## NEEDED TO TRACE A LINE ON SCREEN
+   # points = []
+   # pos = car.position
+   # orient = car.orientation
+   # pos_behind = pos - orient * 20.0
+   # points.append((int(pos_behind.x), int(pos_behind.y)))
+   # points.append((car_x, car_y))
 
-    #Limit the framerate
-    dtime_ms = clock.tick(fpsLimit)
-    dtime = dtime_ms/1000.0
+    while car.crashed == False and runMe == True:
 
-    screen.fill(white)
+        #get user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runMe = False
 
-    #draw obstacles
-    pygame.draw.lines(screen, (0,0,0), True, obstacle1, 5)
-    pygame.draw.lines(screen, (0,0,0), True, obstacle2, 5)
+        #Limit the framerate
+        dtime_ms = clock.tick(fpsLimit)
+        dtime = dtime_ms/1000.0
 
-    car.move(dtime)
-    car.sense(obstacle1)
-    car.sense(obstacle2)
-    car.detect_collision(obstacle1)
-    car.detect_collision(obstacle2)
-    car.display(screen)
-    car.reset_sensors()
+        keys = pygame.key.get_pressed()
 
-    screen.unlock()
+        if (keys[pygame.K_a] ==True and keys[pygame.K_f] ==True) or (keys[pygame.K_a] ==False and keys[pygame.K_f] ==False):
+            steering_rate = 0.0        
+        elif keys[pygame.K_a] ==True:
+            steering_rate = -1 * max_steering_rate
+        elif keys[pygame.K_f] ==True:
+            steering_rate = max_steering_rate
 
-    pygame.display.flip()
+        if (keys[pygame.K_UP] ==True and keys[pygame.K_DOWN] ==True) or (keys[pygame.K_UP] ==False and keys[pygame.K_DOWN] ==False):
+            accel = 0.0        
+        elif keys[pygame.K_UP] ==True:
+            accel = max_accel
+        elif keys[pygame.K_DOWN] ==True:
+            accel = -1*max_accel
+        car.control(accel, steering_rate)
 
+        screen.fill((255,255,255))
+
+        obstacles = [left_barrier,right_barrier]
+
+        #draw obstacles
+        for obstacle in obstacles:
+            pygame.draw.lines(screen, (0,0,0), True, obstacle, 5)
+
+        car.update(dtime, obstacles)
+        car.display(screen)
+
+        ## NEEDED TO TRACE A LINE ON SCREEN
+       # timer += dtime
+       # if timer - old_timer > clock_tick:
+       #    pos = car.position
+       #    orient = car.orientation
+       #    pos_behind = pos - orient * 20.0
+
+       #    points.append((int(pos_behind.x), int(pos_behind.y)))
+
+       #    #print (pos_behind.x, pos_behind.y)
+       #    old_timer = timer
+
+        screen.unlock()
+        pygame.display.flip()
+        ## NEEDED TO TRACE A LINE ON SCREEN
+    #print points
 pygame.quit()
 sys.exit()
